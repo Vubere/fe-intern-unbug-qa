83a8ec19d84501935a6584acc9f0d517ffa3aa04
Victor Ubere <victorubere@gmail.com>
2023-07-20 12:38:50 +0100

    feat/ initial commit

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    
+  </header>
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <h1></h1>
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'HomePage',
+  head: {
+    title: 'UnbugQA | Home',
+    meta: [
+      {
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'This is Nuxt.js project made in response to an assessment test from UnbugQA for the position of Frontend Developer'
+      }
+    ]
+  }
+})
+</script>
